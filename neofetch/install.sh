647b81a67b77cd12883abce33d322ed0b179658e
Allan Deutsch <allan@allandeutsch.com>
2025-02-04 14:39:57 -0800

    removed neofetch (deprecated); added filenames at top of nvim windows via lualine

--- neofetch/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-brew install neofetch
-
-# Run neofetch whenever a new zsh session is started
-echo "neofetch" >> ~/.zshrc
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,18 +1,31 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "rose-pine",
-      },
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "rose-pine",
+				globalstatus = true,
+				always_show_tabline = true,
+				disabled_filetypes = { winbar = { "NvimTree", "neo-tree", "dashboard", "Outline" } },
+			},
+			-- draw a bar with the filename at the top of the focused file buffer window
+			winbar = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+			-- draw a bar with the filename at the top of each unfocused file buffer window
+			inactive_winbar = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
