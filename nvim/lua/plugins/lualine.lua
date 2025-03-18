81b2893fb0d77cbdec85cbe59751dd9fd655a80b
Allan Deutsch <allan@allandeutsch.com>
2025-03-17 17:33:01 -0700

    lualine at top of window more clearly shows buffer is modified

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,31 +1,40 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "rose-pine",
-				globalstatus = true,
-				always_show_tabline = true,
-				disabled_filetypes = { winbar = { "NvimTree", "neo-tree", "dashboard", "Outline" } },
-			},
-			-- draw a bar with the filename at the top of the focused file buffer window
-			winbar = {
-				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
-				},
-			},
-			-- draw a bar with the filename at the top of each unfocused file buffer window
-			inactive_winbar = {
-				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
-				},
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "rose-pine",
+        globalstatus = true,
+        always_show_tabline = true,
+        disabled_filetypes = { winbar = { "NvimTree", "neo-tree", "dashboard", "Outline" } },
+      },
+      -- draw a bar with the filename at the top of the focused file buffer window
+      winbar = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+            color = function()
+              local colors = require("lualine.themes." .. vim.g.colors_name)
+
+              return {
+                fg = vim.bo.modified and colors.insert.a.fg or colors.inactive.c.fg,
+                bg = vim.bo.modified and colors.insert.a.bg or colors.inactive.c.bg,
+                gui = vim.bo.modified and "bold" or "NONE",
+              }
+            end,
+          },
+        },
+      },
+      -- draw a bar with the filename at the top of each unfocused file buffer window
+      inactive_winbar = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+      },
+    })
+  end,
 }
