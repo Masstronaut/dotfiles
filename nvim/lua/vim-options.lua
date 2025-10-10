bde9765bc4429f6e2602fb1a761272b9a4451b49
Allan Deutsch <allan@allandeutsch.com>
2025-10-10 15:49:51 -0700

    refactor(nvim): update tab width to 4 spaces

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,8 +1,7 @@
 -- Basic vim editor config options
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 vim.o.swapfile = false -- Disable swap files
 vim.wo.relativenumber = true -- Enable relative line numbers
